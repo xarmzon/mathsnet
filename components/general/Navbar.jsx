7dc6d427bd0e92c01bcf4d9f4c97d1a96a110f5b
RastaXarm <xarmzon@gmail.com>
2021-08-01 05:03:05 +0100

    fixed navbar items

--- components/general/Navbar.jsx
+++ components/general/Navbar.jsx
@@ -18,9 +18,9 @@ const Navbar = ({ navState }) => {
     <div
       className={`absolute w-full ${
         mobileNavOff ? "-top-full" : "top-0"
-      } left-0 transition-all duration-500  pt-6 bg-primary pb-10 md:pb-0 md:relative md:pt-0 md:w-auto`}
+      } left-0 transition-all duration-500  pt-6 bg-primary pb-10 md:pb-0 md:bg-transparent md:relative md:pt-0 md:w-auto`}
     >
-      <div className="flex flex-col items-center space-y-5 md:flex-row md:space-x-4 relative">
+      <div className="flex flex-col items-center space-y-5 md:flex-row md:space-x-4 relative md:space-y-0">
         <FaTimes
           className="absolute -top-1 right-7 cursor-pointer md:hidden"
           onClick={closeMobileNav}
