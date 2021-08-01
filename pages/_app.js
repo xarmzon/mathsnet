a196044dfd8d0db384697bda2d5059b2f26c075d
RastaXarm <xarmzon@gmail.com>
2021-08-01 04:55:22 +0100

    added _document.js and favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
 import "../styles.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Home | MathsNet</title>
+        <meta
+          data-hid="description"
+          name="description"
+          content="MathsNet, Online maths learning portal. MathsNet is a platform that teaches all the curriculum Mathematics at Primary, Secondary and A levels Mathematics."
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
---
+++ pages/_document.js
@@ -0,0 +1,25 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <title>MathsNet - Home of Mathematics</title>
+          <link rel="icon" href="/favicon.png" type="image/png" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
