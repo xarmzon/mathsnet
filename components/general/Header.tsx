395f0b2d389c0581cf31700c7d22823c01bc28f1
RastaXarm <xarmzon@gmail.com>
2021-10-29 02:14:26 +0100

    update site single page class

--- components/general/Header.tsx
+++ components/general/Header.tsx
@@ -2,7 +2,7 @@ import Logo from "./Logo";
 import Navbar from "./Navbar";
 import { HiOutlineViewGrid } from "react-icons/hi";
 import { useState } from "react";
-const Header = ({ type = "primary" }) => {
+const Header = ({ type = "primary", fixed = false }) => {
   const classData = {
     color: type === "primary" ? "text-white" : "text-primary",
     bg: type === "primary" ? "bg-primary" : "bg-gray-50",
@@ -12,7 +12,11 @@ const Header = ({ type = "primary" }) => {
   const [navStateOff, setNavStateOff] = useState(true);
   const openMobileNav = () => setNavStateOff((prev) => !prev);
   return (
-    <div className={`${classData.bg} ${classData.color} h-[60px] z-50`}>
+    <div
+      className={`${fixed && "fixed top-0 left-0 right-0"} ${classData.bg} ${
+        classData.color
+      } h-[60px] z-50`}
+    >
       <div className="flex justify-between items-center container mx-auto p-5 pt-3">
         <Logo type={logoType()} />
         <HiOutlineViewGrid
--- pages/[class_slug]/index.tsx
+++ pages/[class_slug]/index.tsx
@@ -6,12 +6,15 @@ import { formatPrice } from "../../utils";
 import { ROUTES, CONSTANTS } from "../../utils/constants";
 import Footer from "../../components/general/Footer";
 import Header from "../../components/general/Header";
+import Loader from "../../components/general/Loader";
 import LinkButton, { ETypes } from "../../components/general/LinkButton";
 import { useAppSelector, useAppDispatch } from "../../redux/store";
 import { NextSeo } from "next-seo";
 import { FaAngleRight } from "react-icons/fa";
 import { FiBookOpen, FiX, FiBook, FiMessageCircle } from "react-icons/fi";
 import Link from "next/link";
+import katex from "katex";
+import dateformat from "dateformat";
 interface ITopics {
   id: string;
   title: string;
@@ -30,7 +33,11 @@ interface IClassData {
 }
 
 const ClassViewPage = ({ classD }) => {
-  const [currentTab, setCurrentTab] = useState<number>(1)
+  const dispatch = useAppDispatch();
+
+  const { user, loading, loggedIn } = useAppSelector((state) => state.auth);
+
+  const [currentTab, setCurrentTab] = useState<number>(1);
   const [showMobileTopicsNav, setShowMobileTopicsNav] =
     useState<boolean>(false);
   const [classData, setClassData] = useState<IClassData>(() => {
@@ -65,15 +72,21 @@ const ClassViewPage = ({ classD }) => {
       };
     }
   });
+
   classData && console.log(classData);
+
+  useEffect(() => {
+    if (katex) window.katex = katex;
+  }, []);
+
   return (
     <div>
       <NextSeo title={classData?.title || "Loading"} />
       <div className="mb-2">
-        <Header />
+        <Header fixed={true} />
       </div>
       <div className="container pt-2">
-        <div className="-space-x-1 flex items-center flex-wrap">
+        <div className="mt-[60px] -space-x-1 flex items-center flex-wrap">
           <span className="flex items-center">
             <LinkButton href="/" txt="Home" type={ETypes.TEXT} />
             <FaAngleRight className="text-sm md:text-lg text-gray-300" />
@@ -105,21 +118,23 @@ const ClassViewPage = ({ classD }) => {
                   )}
                 </span>
                 <div
-                  className={`overflow-hidden ${
+                  className={`overflow-hidden z-40 md:z-auto ${
                     showMobileTopicsNav
-                      ? "fixed top-0 left-0 bottom-0 w-[70%] shadow-md"
+                      ? "fixed top-[60px] left-0 bottom-0 w-[70%] shadow-md"
                       : "w-0"
-                  } bg-gray-50 text-primary md:bg-primary md:text-gray-50 min-h-[100px] md:block md:w-[34%] transition duration-700`}
+                  } bg-gray-50 text-primary md:bg-gray-100 md:text-primary md:min-h-[200px] md:max-h-[200px] md:block md:w-[34%] transition duration-700`}
                 >
                   <div className="space-y-3">
-                    <h4 className="p-5 md:text-lg md:font-bold">Topics List</h4>
+                    <h4 className="p-5 md:text-lg md:font-bold">
+                      Class Topics{" "}
+                    </h4>
                     <ul className="h-full overflow-y-auto divide-y divide-gray-300 md:divide-primary-200">
                       {classData.topics.map((t, i) => (
                         <li
                           key={i}
                           className={`${
                             i > 0 && "pt-3"
-                          } my-3 px-5 text-primary md:text-primary-100 cursor-pointer line-clamp-2`}
+                          } my-3 px-5 text-primary cursor-pointer line-clamp-2`}
                         >
                           <Link href={`/${classData.slug}/${t.slug}`}>
                             <a>{t.title}</a>
@@ -130,30 +145,83 @@ const ClassViewPage = ({ classD }) => {
                   </div>
                 </div>
                 <div className="w-full md:w-[65%]">
-                  <h1 className="font-bold text-2xl md:text-3xl">
-                    {classData.title}
-                  </h1>
                   <img
                     className="w-full object-cover mt-2 rounded"
                     src={classData.thumbnail || "/assets/images/thumbnail.jpg"}
                   />
+                  <p className="mt-1 text-xs md:text-sm text-gray-400">
+                    Added on: {dateformat(classData.createdAt, "fullDate")}
+                  </p>
+                  <h1 className="mt-2 mb-4 font-bold text-xl md:text-2xl">
+                    {classData.title}
+                  </h1>
+                  <div className="text-center w-full flex justify-center p-3">
+                    {loading ? (
+                      <div className="my-2">
+                        <Loader />
+                      </div>
+                    ) : loggedIn ? (
+                      user.userType === CONSTANTS.USER_TYPES.STUDENT && (
+                        <p className="text-gray-100 text-xs">
+                          Check for subscription of student
+                        </p>
+                      )
+                    ) : (
+                      <p className="my-1 text-xs md:text-sm">
+                        Contents for registered users only.{" "}
+                        <Link href={ROUTES.AUTH.SIGNUP}>
+                          <a className="text-red-600">Login/Register</a>
+                        </Link>{" "}
+                        in order to add/access the class topics{" "}
+                      </p>
+                    )}
+                  </div>
                   <div className="mt-4 w-full">
-                    <div className="flex w-full overflow-x-auto space-x-4 min-h-[10px] border-b-2 border-gray-200">
-                      <div className="flex items-center space-x-1">
+                    <div className="flex w-full overflow-x-auto space-x-5 md:space-x-7 min-h-[10px] border-b-2 border-gray-200">
+                      <div
+                        onClick={() => setCurrentTab(1)}
+                        className={`${
+                          currentTab === 1
+                            ? "border-b border-primary text-primary"
+                            : "border-0 text-secondary"
+                        } flex items-center space-x-1 cursor-pointer`}
+                      >
                         <FiBook />
                         <span className="">Description</span>
                       </div>
-                      <div className="flex items-center space-x-1">
+                      <div
+                        onClick={() => setCurrentTab(2)}
+                        className={`${
+                          currentTab === 2
+                            ? "border-b border-primary text-primary"
+                            : "border-0 text-secondary"
+                        } flex items-center space-x-1 cursor-pointer`}
+                      >
                         <FiMessageCircle />
                         <span className="">Reviews</span>
                       </div>
                     </div>
-                    <div>
-                      <div className={`${currentTab === 1 ? "block w-full" : "hidden"}`}>
-                        tab1 contents
+                    <div className="pt-3 mb-3">
+                      <div
+                        className={`${
+                          currentTab === 1 ? "block w-full" : "hidden"
+                        }`}
+                      >
+                        {classData.desc ? (
+                          <div
+                            className="w-full"
+                            dangerouslySetInnerHTML={{ __html: classData.desc }}
+                          ></div>
+                        ) : (
+                          <p>No Description</p>
+                        )}
                       </div>
-                      <div className={`${currentTab === 2 ? "block w-full" : "hidden"}`}>
-                        tab2 contents
+                      <div
+                        className={`${
+                          currentTab === 2 ? "block w-full" : "hidden"
+                        }`}
+                      >
+                        Reviews Tab
                       </div>
                     </div>
                   </div>
