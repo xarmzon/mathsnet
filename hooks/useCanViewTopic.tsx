9fec292e0069c7f89607cd0f6f945807eb7234d0
unknown <xarmzon@gmail.com>
2022-03-30 09:28:47 +0000

    update useCanViewTopic-3

--- hooks/useCanViewTopic.tsx
+++ hooks/useCanViewTopic.tsx
@@ -48,7 +48,7 @@ const useCanViewTopic = ({ classSlug, classPrice }: IUseCanViewTopic) => {
       setLoading(false);
     };
     checker();
-  }, [loggedIn]);
+  }, [loggedIn, classPrice, classSlug]);
 
   return {
     canView,
